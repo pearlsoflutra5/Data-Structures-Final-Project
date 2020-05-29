6514dcbe88d630bbdb8db4316710aab20646b2f3
pearlsoflutra5 <pearlsoflutra5@gmail.com>
2020-05-29 09:44:13 -0600

    fixed client and server files

--- finalClient.java
+++ finalClient.java
@@ -5,6 +5,7 @@ Description: Client which connects to Server.
 */
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -17,72 +18,103 @@ import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import java.io.*;
 import java.net.*;
+import javafx.scene.input.*;
+import javafx.event.*;
+
 
 public class finalClient extends Application {
 	private TextField ltitle = new TextField();
 	private TextField lauthor = new TextField();
-	private TextField lgenre = new TextField();
 	private Button btSearch= new Button("Search");
-	DataOutputStream toServer = null;
-	DataInputStream fromServer = null;
-
-	
+	DataOutputStream toServer;
+	DataInputStream fromServer;
 	private TextArea ta = new TextArea();
 	
 	@Override
 	public void start(Stage primaryStage) {
 		ta.setWrapText(true);
+		ta.setEditable(false);
 		
 		GridPane gridPane = new GridPane();
-		gridPane.add(new Label("Title"), 0, 0);
-		gridPane.add(new Label("Author  "), 0, 1);
-		//gridPane.add(new Label("Genre"), 0, 2);
-		gridPane.add(ltitle, 1, 0);
-		gridPane.add(lauthor, 1, 1);
-		//gridPane.add(lgenre, 1, 2);
-		gridPane.add(btSearch, 2, 1);
+		gridPane.add(new Label("Type 'all' to see the whole library."), 1, 0);
+		gridPane.add(new Label("Title"), 0, 1);
+		gridPane.add(new Label("Author"), 0, 2);
+		gridPane.add(ltitle, 1, 1);
+		gridPane.add(lauthor, 1, 2);
+		gridPane.add(btSearch, 2, 2);
 		
 		ltitle.setAlignment(Pos.BASELINE_LEFT);
 		lauthor.setAlignment(Pos.BASELINE_LEFT);
-		//lgenre.setAlignment(Pos.BASELINE_LEFT);
-		
 		ltitle.setPrefColumnCount(10);
 		lauthor.setPrefColumnCount(10);
-		//lgenre.setPrefColumnCount(10);
 						
 		BorderPane pane = new BorderPane();
-		pane.setCenter(new ScrollPane(ta));
+		pane.setCenter(new BorderPane(ta));
 		pane.setTop(gridPane);
-		
 		
 		Scene scene = new Scene(pane, 545, 240);
 		primaryStage.setTitle("Client Search Bar"); 
 		primaryStage.setScene(scene); 
 		primaryStage.show(); 
-				
-		btSearch.setOnAction(e -> {
-			try {
-				String title = ltitle.getText().trim();
-				String author = lauthor.getText().trim();
-				//String genre = lgenre.getText().trim();
-				toServer.writeUTF(title);
-				toServer.writeUTF(author);
-				toServer.flush();
-			} 
-			catch (IOException ex) {
-				System.err.println(ex);
-			}
-
-		});
-
+		
 		try {
 			Socket socket = new Socket("localhost", 8000);
 			fromServer = new DataInputStream(socket.getInputStream());
 			toServer = new DataOutputStream(socket.getOutputStream());
+			new Thread(() -> {
+				try {
+					while (true) { 
+						String serverText = fromServer.readUTF();
+						Platform.runLater(() -> ta.appendText(serverText));	
+					}        
+				} 
+				catch (IOException ex) {
+						ex.printStackTrace();
+				}
+			}).start();
+
 		}
 		catch (IOException ex) {
 			ta.appendText(ex.toString() + '\n');
 		}
+			
+		btSearch.setOnAction(e -> {
+			try {
+				String title = ltitle.getText().trim();
+				String author = lauthor.getText().trim();
+				if (title != null && author != null){
+					toServer.writeUTF(title);
+					toServer.writeUTF(author);
+					toServer.flush();
+					ltitle.setText("");
+					lauthor.setText("");
+				}			
+			} 
+			catch (IOException ex) {
+				System.err.println(ex);
+			}
+		});
+		gridPane.setOnKeyPressed(event -> {
+			if (event.getCode() == KeyCode.ENTER) {
+				try {
+					String title = ltitle.getText().trim();
+					String author = lauthor.getText().trim();
+					if (title != null && author != null){
+						toServer.writeUTF(title);
+						toServer.writeUTF(author);
+						toServer.flush();
+						ltitle.setText("");
+						lauthor.setText("");
+					}
+					
+				} 
+				catch (IOException ex) {
+					System.err.println(ex);
+				}			
+			}
+		});
+
+
 	}
 	public static void main(String[] args) {
 		launch(args);
--- finalServer.java
+++ finalServer.java
@@ -16,31 +16,60 @@ import java.net.*;
 
 public class finalServer extends Application {
 	private TextArea ta = new TextArea();
+	DataOutputStream toServer;
+	DataInputStream fromServer;
+	public int index;
+	public ArrayList<String> titles = new ArrayList<String>();
+	public ArrayList<String> authors = new ArrayList<String>();
+	public ArrayList<Integer> indexes = new ArrayList<Integer>();
 
 	@Override 
 	public void start(Stage primaryStage) {
 		ta.setWrapText(true);
-		//ta.setDisable(true);
+		ta.setEditable(false);
 		
 		Scene scene = new Scene(new ScrollPane(ta), 545, 185);
 		primaryStage.setTitle("My Library"); 
 		primaryStage.setScene(scene);
-		primaryStage.show();
+		primaryStage.show();		
 		
 		new Thread(() -> {
 			try {
 				ServerSocket serverSocket = new ServerSocket(8000);
-				Platform.runLater(() -> ta.appendText("Welcome to my library! \n"));
-				Socket socket = serverSocket.accept();
-				DataInputStream inputFromClient = new DataInputStream(socket.getInputStream());
-				DataOutputStream outputToClient = new DataOutputStream(socket.getOutputStream());
+				Platform.runLater(() -> ta.appendText("Welcome to my library of classics! \n"));
 				
+				Socket socket = serverSocket.accept();
+				DataInputStream toServer = new DataInputStream(socket.getInputStream());
+				DataOutputStream fromServer = new DataOutputStream(socket.getOutputStream());
+
 				while (true) {
-					String title = inputFromClient.readUTF();
-					String author = inputFromClient.readUTF();
-					Platform.runLater(() -> {
-		
-					});
+					String titleKeyword = toServer.readUTF().trim();
+					String authorKeyword = toServer.readUTF().trim();
+					search(titleKeyword, authorKeyword);
+					if (!indexes.isEmpty()){
+						for(int i = 0; i<indexes.size(); i++) {
+							index = indexes.get(i);
+							for(int j = 0; j< libraryList[index].length; j++){
+								String string = libraryList[index][j];
+								Platform.runLater(() -> ta.appendText(string + " "));
+								fromServer.writeUTF(libraryList[index][j] + " ");
+							}
+							Platform.runLater(() -> ta.appendText("\n"));
+							fromServer.writeUTF("\n");
+							fromServer.flush();	
+						}
+						Platform.runLater(() -> ta.appendText("-----------------------------------------\n"));
+						fromServer.writeUTF("-----------------------------------------\n");
+					}
+					else {
+						Platform.runLater(() -> {
+							ta.appendText("There is no match for your search.\n");
+							ta.appendText("-----------------------------------------\n");
+						});
+						
+						fromServer.writeUTF("There is no match for your search.\n");
+						fromServer.writeUTF("-----------------------------------------\n");
+					}
 				}
 			}
 			catch(IOException ex) {
@@ -48,6 +77,54 @@ public class finalServer extends Application {
 			}
 		}).start();
 	}
+
+	public  void search(String t, String a){
+		indexes.clear();
+		String locator = t.toLowerCase();
+		String locator2 = a.toLowerCase();
+		if (locator != null && locator.equals("all") || locator2 != null && locator2.equals("all")){
+			for(int i = 0; i<libraryList.length; i++){
+				indexes.add(i);
+			}
+		}
+		else{
+			for(int i = 0; i<libraryList.length; i++){
+				if(libraryList[i][0].toLowerCase().equals("''" + locator + "''")){
+					indexes.add(i);
+				}
+				else if (libraryList[i][1].toLowerCase().equals("by " + locator2)){
+					indexes.add(i);
+				}
+			}
+		}			
+	}
+	String[][] libraryList = {
+		{"''The Odyssey''", "by Homer",}, 
+		{"''The Iliad''", "by Homer"},
+		{"''To Win Her Heart''", "by Karen Whitemeyer"}, 
+		{"''A Bride for Keeps''", "by Melissa Jagears"},
+		{"''To Kill a Mockingbird''", "by Harper Lee"},
+		{"''Pride and Prejudice''", "by Jane Austin"},
+		{"''Emma''", "by Jane Austin"},
+		{"''1984''", "by George Orwell"},
+		{"''Animal Farm''", "by George Orwell"},
+		{"''Jane Eyre''", "by Charlotte Bronte"},
+		{"''The Catcher in the Rye''", "by J D Salinger"},
+		{"''The Great Gatsby''", "by F Scott Fitzgerald"},
+		{"''Little Women''", "by Louisa May Alcott"},
+		{"''The Adventures of Huckleberry Finn''", "by Mark Twain"},
+		{"''Great Expectations''", "by Charles Dickens"},
+		{"''Fahrenheit 451''", "by Ray Bradbury"},
+		{"''The Grapes of Wrath''", "by John Steinbeck"},
+		{"''Of Mice and Men''", "by John Steinbeck"},
+		{"''Catch 22''", "by Joseph Heller"},
+		{"''The Scarlet Letter''", "by Nathaniel Hawthorne"},
+		{"''Tale of Two Cities''", "by Charles Dickens"},
+		{"''War and Peace''", "by Leo Tolstoy"},
+		{"''The Secret Garden''", "by Frances Hodgess Burnett"},
+		{"''Old Yeller''", "by Fred Gipson"},
+		{"''Savage Sam''", "by Fred Gipson"},
+	};	
 	public static void main(String[] args) {
 		launch(args);
 	}
